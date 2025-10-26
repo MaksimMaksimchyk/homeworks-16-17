5b887df365b5e39fab067a592b59d972f853a5fd
MaksimMaksimchyk <maksim.maksimchyk@yandex.by>
2025-10-26 20:23:52 +0300

    Homework16 done

--- app/src/main/java/com/example/homeworksapp/homework16/Homework16MainActivity.kt
+++ app/src/main/java/com/example/homeworksapp/homework16/Homework16MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.homeworksapp.homework16
 
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.wifi.WifiManager
